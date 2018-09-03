db9f592fc21acc0350e5e48ce8cd5531611443af
jesslalanne <jessica.lalanne@gmail.com>
2018-09-03 21:26:18 +0200

    Update Style App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact List Manager" />
+        <div className="container">
         <Contact name="Jessica Lalanne" email="jessica.lalanne@gmail.com" phone="06 49 39 79 13" />
         <Contact name="Yiskah Teixeira" email="yiskah.teixeira@gmail.com" phone="06 98 76 54 32" />
-        
+        </div>
       </div>
     );
   }
