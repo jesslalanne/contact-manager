09efee5769052ebf01494619442a5e9585db50b6
jesslalanne <jessica.lalanne@gmail.com>
2018-09-03 21:24:07 +0200

    Add Header and Contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact List Manager" />
+        <Contact name="Jessica Lalanne" email="jessica.lalanne@gmail.com" phone="06 49 39 79 13" />
+        <Contact name="Yiskah Teixeira" email="yiskah.teixeira@gmail.com" phone="06 98 76 54 32" />
+        
       </div>
     );
   }
